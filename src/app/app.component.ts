9e569f552399427cd638c74795385865a33e2407
Stanley <stallotey@mix.wvu.edu>
2022-02-10 08:41:48 -0500

    Restructured card product list 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { mock_product_list } from './Body/mock-product-list';
+import { ProductItemModel } from './Body/product-item-model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 't-mobile-clone';
+  products: ProductItemModel[] = [];
+
+  constructor() {
+    for (var product of mock_product_list) {
+      console.log(product);
+      this.products.push(product);
+    }
+  }
 }
